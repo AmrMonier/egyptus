a53be40918275e0a4fe0567a5cb08dad83c1f15f
Amr Monier <a.monier2107@gmail.com>
2021-12-21 01:40:10 +0200

    styled links with bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <div>
             <h2>By Amr Monier 😎.</h2>
         </div>
-        <a href="http://github.com/AmrMonier/egyptus">The github repository</a>
+        <a href="http://github.com/AmrMonier/egyptus" class="btn btn-outline-secondary my-2">The github repository</a>
+        <a href="/trips" class="btn btn-outline-primary">Explore trips</a>
     </div>
 @endsection
