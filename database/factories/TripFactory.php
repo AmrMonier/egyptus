ffa04cf3f8aa1675122b1702411296c8a5992a5b
Amr Monier <a.monier2107@gmail.com>
2021-12-20 22:50:45 +0200

    added available_seats to  the trip factory file

--- database/factories/TripFactory.php
+++ database/factories/TripFactory.php
@@ -18,13 +18,15 @@ class TripFactory extends Factory
        
         $start_date = $this->faker->dateTimeBetween('+5 days','+1 years');
         $end_date = $start_date->add(new DateInterval(sprintf('P%dD', rand(2, 15))));
+        $seats = rand(20,100);
         return [
             'name' => $this->faker->sentence(),
             'details' => $this->faker->paragraph(),
             'price' => $this->faker->numberBetween(200,5000),
             'start_date' => $start_date,
             'end_date' => $end_date, 
-            'seats' => rand(20,100),
+            'seats' => $seats,
+            'available_seats' => $seats,
             'program' => $this->faker->paragraph()
         ];
     }
